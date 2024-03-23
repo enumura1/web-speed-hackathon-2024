cec7a581dd29e9477620c104c76fdb22f1790a52
enumura1 <akirayuto0718@gmail.com>
2024-03-24 01:37:48 +0900

    fix:unix command to windows command

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
 const _Wrapper = styled.div`
-  aspect-ratio: 16 / 9;
   width: 100%;
+  aspect-ratio: 16 / 9;
+  object-fit: fill;
 `;
 
 const _Image = styled.img`
-  display: inline-block;
   width: 100%;
   aspect-ratio: 16 / 9;
+  display: inline-block;
 `;
 
 export const HeroImage: React.FC = () => {
